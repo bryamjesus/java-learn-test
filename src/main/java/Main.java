aece195345bb75919aaeb61625efa513a30f13c4
Bryam Jesus Talledo Garcia <bryam.talledo@outlook.com>
2025-07-24 14:22:31 -0500

    Feat: learn mockito

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import mokito.Game;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        simpleTestJunit();
+        game();
+    }
+
+    private static void simpleTestJunit()
+    {
         double salary = -1000;
         System.out.println(MoneyUtil.format(salary, "€"));
         System.out.println("Hello world");
+    }
+
+    private static void game()
+    {
+        Game game = new Game();
+        game.play();
     }
 }
